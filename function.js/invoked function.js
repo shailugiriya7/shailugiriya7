bacb0ce943433e9ffbeaf4876450829a73a38356
shailugiriya7 <142213894+shailugiriya7@users.noreply.github.com>
2023-08-26 02:06:41 +0530

    Delete function.js directory

--- function.js/invoked function.js
+++
@@ -1,9 +0,0 @@
-
-// imediatly invoked function expression
-(function one(){
-    console.log("shailu");
-})();  // Output: "shailu"
-
-((two) => {
-    console.log(`babu gobar, ${two}`);
-})('babu');  // Output: "babu gobar, babu"
